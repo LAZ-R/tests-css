852a2e09f0ce09a60b492fb62550a4f317288a07
LAZ-R <crazydrawer62@gmail.com>
2022-08-02 14:57:49 +0200

    v 0.04 : window.location

--- js/components/header/header.component.js
+++ js/components/header/header.component.js
@@ -4,13 +4,26 @@ export const render = () => {
         '<span class="header-logo" alt="app logo">ES6 pwa</span>' +
         '<nav class="header-nav">' +
             '<ul class="header-nav-ul">' +
+            '<li class="header-nav-item">' +
+                    '<button id="homeButton">Home</button>' +
+                '</li>' +
                 '<li class="header-nav-item">' +
-                    '<a href="./about.html">About</a>' +
+                    '<button id="aboutButton">About</button>' +
                 '</li>' +
             '</ul>' +
         '</nav>';
 
     document.getElementById('body').insertBefore(header, document.getElementById('main'));
+    const homeButton = document.getElementById('homeButton');
+    homeButton.addEventListener('click', () => {
+        window.location = './index.html';
+    });
+    
+    document.getElementById('body').insertBefore(header, document.getElementById('main'));
+    const aboutButton = document.getElementById('aboutButton');
+    aboutButton.addEventListener('click', () => {
+        window.location = './about.html';
+    });
 
 
     const ghostHeader = document.createElement('div');
