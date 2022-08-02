17eb9ec98289c049d2da1147d4ca2d70e00d810d
LAZ-R <crazydrawer62@gmail.com>
2022-08-02 15:12:47 +0200

    v 0.0.5 : revert to links + webkit css

--- js/components/header/header.component.js
+++ js/components/header/header.component.js
@@ -4,26 +4,13 @@ export const render = () => {
         '<span class="header-logo" alt="app logo">ES6 pwa</span>' +
         '<nav class="header-nav">' +
             '<ul class="header-nav-ul">' +
-            '<li class="header-nav-item">' +
-                    '<button id="homeButton">Home</button>' +
-                '</li>' +
                 '<li class="header-nav-item">' +
-                    '<button id="aboutButton">About</button>' +
+                    '<a href="./about.html">About</a>' +
                 '</li>' +
             '</ul>' +
         '</nav>';
 
     document.getElementById('body').insertBefore(header, document.getElementById('main'));
-    const homeButton = document.getElementById('homeButton');
-    homeButton.addEventListener('click', () => {
-        window.location = './index.html';
-    });
-    
-    document.getElementById('body').insertBefore(header, document.getElementById('main'));
-    const aboutButton = document.getElementById('aboutButton');
-    aboutButton.addEventListener('click', () => {
-        window.location = './about.html';
-    });
 
 
     const ghostHeader = document.createElement('div');
