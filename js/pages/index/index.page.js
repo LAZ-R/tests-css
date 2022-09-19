f3cf8e4d53979f9d07bff0d402b4797019892350
LAZ-R <crazydrawer62@gmail.com>
2022-09-19 19:28:37 +0200

    new v0.0.1

--- js/pages/index/index.page.js
+++ js/pages/index/index.page.js
@@ -1,6 +1,4 @@
-import * as COMPONENT_FOOTER from '../../components/footer/footer.component.js'
 import * as SERVICE_PWA from '../../services/pwa.service.js';
-import * as COMPONENT_HEADER from "../../components/header/header.component.js";
 
 const pageTitle = 'ES6 pwa';
 
@@ -8,59 +6,21 @@ const renderView = () => {
 
     SERVICE_PWA.setHTMLTitle(pageTitle);
     
-    const page = document.createElement('div');
-    page.setAttribute('id', 'page');
-    page.setAttribute('class', 'page');
-    page.appendChild(document.createElement('h1')).innerHTML =
-        'Welcome';
-    page.appendChild(document.createElement('p')).innerHTML =
-        'This is a template for a Vanilla JavaScript ES6 static PWA with modules, hosted on GitHub Pages.';
-
-    page.appendChild(document.createElement('h2')).innerHTML =
-        'How ?';
-    page.appendChild(document.createElement('p')).innerHTML =
-        'Everything is made in house.<br>' +
-        'No library. No framework.<br>' +
-        'Nothing.<br><br>' +
-        'Not even Bootstrap. You don\'t need Bootstrap.<br>' +
-        'Just learn CSS ♥</br>';
-
-    page.appendChild(document.createElement('h2')).innerHTML =
-        'Why ?';
-    page.appendChild(document.createElement('p')).innerHTML =
-        'Vanilla HTML, vanilla CSS, vanilla JS, the point here is no <b>compilation</b>, no <b>versioning problems</b>, no <b>framework specific synthax</b> to learn.<br>' +
-        'Basically a POC, not gonna lie.';
-    page.appendChild(document.createElement('h3')).innerHTML =
-        'Also'
-    page.appendChild(document.createElement('p')).innerHTML =
-        'Everything is here and customizable.<br>You don\'t like the way I handle my CSS ? Just change it, it\'s right there.';
-
-
-    page.appendChild(document.createElement('p')).innerHTML =
-        'This is a template for a Vanilla JavaScript ES6 static PWA with modules, hosted on GitHub Pages.';
-
-    page.appendChild(document.createElement('h2')).innerHTML =
-        'How ?';
-    page.appendChild(document.createElement('p')).innerHTML =
-        'Everything is made in house.<br>' +
-        'No library. No framework.<br>' +
-        'Nothing.<br><br>' +
-        'Not even Bootstrap. You don\'t need Bootstrap.<br>' +
-        'Just learn CSS ♥</br>';
-
-    page.appendChild(document.createElement('h2')).innerHTML =
-        'Why ?';
-    page.appendChild(document.createElement('p')).innerHTML =
-        'Vanilla HTML, vanilla CSS, vanilla JS, the point here is no <b>compilation</b>, no <b>versioning problems</b>, no <b>framework specific synthax</b> to learn.<br>' +
-        'Basically a POC, not gonna lie.';
-    page.appendChild(document.createElement('h3')).innerHTML =
-        'Also'
-    page.appendChild(document.createElement('p')).innerHTML =
-        'Everything is here and customizable.<br>You don\'t like the way I handle my CSS ? Just change it, it\'s right there.'
+    const topHalf = document.createElement('div');
+    topHalf.setAttribute('id', 'topHalf');
+    topHalf.setAttribute('class', 'fiftyPercent topHalf');
+    topHalf.appendChild(document.createElement('span')).innerHTML =
+        'coucou';
     
-    document.getElementById('main').appendChild(page);
+    document.getElementById('main').appendChild(topHalf);
+
+    const bottomHalf = document.createElement('div');
+    bottomHalf.setAttribute('id', 'bottomHalf');
+    bottomHalf.setAttribute('class', 'fiftyPercent bottomHalf');
+    bottomHalf.appendChild(document.createElement('span')).innerHTML =
+        'coucou';
+    
+    document.getElementById('main').appendChild(bottomHalf);
 }
 
-COMPONENT_HEADER.render();
 renderView();
-COMPONENT_FOOTER.render();
