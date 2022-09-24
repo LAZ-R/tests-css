d5faa737ec80acac0d2435550ff1dea9690e923e
LAZ-R <crazydrawer62@gmail.com>
2022-09-24 19:05:42 +0200

    v 0.0.15 : footer component

--- js/components/footer/footer.component.js
+++ js/components/footer/footer.component.js
@@ -1,13 +1,6 @@
 import * as SERVICE_PWA from '../../services/pwa.service.js';
-
+const year = new Date().getFullYear();
 export const renderView = () => {
-    const footer = document.getElementById('footer');
-
-    const footerAboutLink = document.createElement('a');
-    footerAboutLink.setAttribute('href', './about.html');
-    footerAboutLink.setAttribute('class', 'footer-about-link');
-    footerAboutLink.innerHTML = 
-        `About v${SERVICE_PWA.getAppVersionNumber()}`
-
-    footer.appendChild(footerAboutLink);
+    document.getElementById('footer').innerHTML =
+    `&copy; ${year} - v${SERVICE_PWA.getAppVersionNumber()} - <a href="./about.html">About</a>`;
 }
--- js/data/pwa.data.js
+++ js/data/pwa.data.js
@@ -4,5 +4,5 @@ export const MAX_SCREEN_SIZE = {
     Laptop: 1919
 }
 
-export const VERSION_NUMBER = '0.0.14';
-// v 0.0.14 : about page
+export const VERSION_NUMBER = '0.0.15';
+// v 0.0.15 : footer component
